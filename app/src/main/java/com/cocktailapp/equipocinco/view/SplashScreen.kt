29c385df6aac24ae373875ed47bbe60cce1e68d9
Deisy Melo <deisymelo0127@gmail.com>
2024-06-03 11:53:25 -0500

    splash-screen view

---
+++ app/src/main/java/com/cocktailapp/equipocinco/view/SplashScreen.kt
@@ -0,0 +1,23 @@
+package com.cocktailapp.equipocinco.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.cocktailapp.equipocinco.R
+
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },5000)
+    }
+}
