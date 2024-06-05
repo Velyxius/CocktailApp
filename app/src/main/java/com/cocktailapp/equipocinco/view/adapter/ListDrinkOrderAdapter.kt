206dd6b3fd1a8041a3cc345fbd7e71a56a1b9737
daniel98 <daniel.andres.caicedo@correounivalle.edu.co>
2024-06-04 23:09:06 -0500

    Funciones de borrar cocteles y pedido

---
+++ app/src/main/java/com/cocktailapp/equipocinco/view/adapter/ListDrinkOrderAdapter.kt
@@ -0,0 +1,38 @@
+package com.cocktailapp.equipocinco.view.adapter
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.recyclerview.widget.RecyclerView
+import com.cocktailapp.equipocinco.R
+import com.cocktailapp.equipocinco.databinding.ItemListDrinkBinding
+import com.cocktailapp.equipocinco.model.Order
+import com.cocktailapp.equipocinco.view.viewholder.ListDrinkOrderViewHolder
+import com.cocktailapp.equipocinco.viewmodel.OrderViewModel
+
+
+class ListDrinkOrderAdapter (private val order: Order,
+                             private val listOrder: MutableList<MutableList<String>>,
+                             private val navController: NavController,
+                             private val orderViewModel: OrderViewModel
+):RecyclerView.Adapter<ListDrinkOrderViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListDrinkOrderViewHolder {
+        val binding = ItemListDrinkBinding.inflate(LayoutInflater.from(parent.context),parent, false)
+        return ListDrinkOrderViewHolder(binding,navController,this)
+    }
+
+    override fun getItemCount(): Int {
+        return listOrder.size
+    }
+
+    override fun onBindViewHolder(holder: ListDrinkOrderViewHolder, position: Int) {
+        val listDrink = listOrder[position]
+        holder.setItemListDrink(order,position,listDrink,orderViewModel)
+    }
+
+    fun removeItem(position: Int) {
+        notifyItemRemoved(position)
+        notifyItemRangeChanged(position, listOrder.size)
+    }
+}
---
+++ app/src/main/java/com/cocktailapp/equipocinco/view/fragment/DeleteOrderFragment.kt
@@ -0,0 +1,63 @@
+package com.cocktailapp.equipocinco.view.fragment
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.cocktailapp.equipocinco.R
+import com.cocktailapp.equipocinco.databinding.FragmentDeleteOrderBinding
+import com.cocktailapp.equipocinco.model.Order
+import com.cocktailapp.equipocinco.viewmodel.OrderViewModel
+
+
+class DeleteOrderFragment : Fragment() {
+
+    private lateinit var binding: FragmentDeleteOrderBinding
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var receivedOrder: Order
+    private val orderViewModel : OrderViewModel by viewModels()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentDeleteOrderBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        sharedPreferences = requireActivity().getSharedPreferences("shared", Context.MODE_PRIVATE)
+        setup()
+    }
+
+    private fun setup() {
+        binding.btnEliminar.setOnClickListener {
+            deleteDrink()
+        }
+
+        binding.btnCancelar.setOnClickListener{
+            val bundle = Bundle()
+            val receivedBundle = arguments
+            receivedOrder = receivedBundle?.getSerializable("clave") as Order
+            bundle.putSerializable("clave",receivedOrder)
+            findNavController().navigate(R.id.action_deleteOrderFragment_to_detailsOrderFragment,bundle)
+        }
+    }
+
+    private fun deleteDrink() {
+        val receivedBundle = arguments
+        receivedOrder = receivedBundle?.getSerializable("clave") as Order
+        orderViewModel.deleteOrder(receivedOrder)
+        findNavController().navigate(R.id.action_deleteOrderFragment_to_homeOrderFragment)
+    }
+
+
+
+}
